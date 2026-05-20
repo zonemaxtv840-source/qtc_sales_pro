17084b21dcfdc2a6611b96c40dd1501c95a0ed58
zonemaxtv840-source <zonemaxtv840@gmail.com>
2026-05-20 18:09:02 -0400

    Update search_tab.py

--- ui/tabs/search_tab.py
+++ ui/tabs/search_tab.py
@@ -1,1 +1,6 @@
+# ui/tabs/search_tab.py - Temporal
+import streamlit as st
 
+def render_search_tab():
+    st.markdown("### 🔍 BÚSQUEDA INTELIGENTE")
+    st.info("🔄 Módulo en construcción... Pronto disponible")
