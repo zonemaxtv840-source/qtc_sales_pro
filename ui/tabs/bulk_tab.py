0c8a577022df9a7cf5dd1ffd9f475b4b0d436c72
zonemaxtv840-source <zonemaxtv840@gmail.com>
2026-05-20 18:08:52 -0400

    Update bulk_tab.py

--- ui/tabs/bulk_tab.py
+++ ui/tabs/bulk_tab.py
@@ -1,1 +1,7 @@
+# ui/tabs/bulk_tab.py - Temporal
+import streamlit as st
 
+def render_bulk_tab():
+    st.markdown("### 📦 MODO MASIVO (Bulk)")
+    st.info("🔄 Módulo en construcción... Pronto disponible")
+    st.write("Modo actual:", st.session_state.modo)
